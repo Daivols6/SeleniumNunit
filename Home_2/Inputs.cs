1d56e61fa5d310498fdaf50718cef0427332cfc4
Daivols6 <daivols@internet.ru>
2023-06-02 22:08:28 +0300

    Add part  inputs and SortableDataTablesTests

---
+++ Home_2/Inputs.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Home_2;
+
+/*Inputs - Проверить на возможность ввести различные цифровые и
+нецифровые значения, используя Keys.ARROW_UP И
+Keys.ARROW_DOWN
+i.
+Локатор By.tagName(“input”)*/
+namespace Home_2
+{
+    public class Inputs:BaseTest
+    {
+        [Test(Description ="InputTests")]
+        public void InputTest()
+        {
+            driver.FindElement(By.LinkText("Inputs")).Click();
+            var input = driver.FindElement(By.TagName("input"));
+            Thread.Sleep(1000);
+            input.SendKeys("100");
+            Thread.Sleep(1000);
+            input.Clear();
+            Thread.Sleep(1000);
+            input.SendKeys("50");
+            Thread.Sleep(1000);
+            input.SendKeys(Keys.ArrowUp + Keys.ArrowUp+ Keys.Enter);
+            Thread.Sleep(1000);
+            input.Clear();
+            Thread.Sleep(1000);
+            input.SendKeys("eE");
+            Thread.Sleep(1000);
+            input.Clear();
+            Thread.Sleep(1000);
+            input.SendKeys("vwvwvwv232");
+            Thread.Sleep(1000);
+        }
+    }
+}
---
+++ Home_2/SortableDataTables.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**Sortable Data Tables - Проверить содержимое нескольких (3-5) ячеек
+таблицы.Использовать xpath типа //table//tr[1]//td[1] - получение первой
+ячейки из первого ряда первой таблицы и так далее*/
+namespace Home_2
+{
+    public class SortableDataTables : BaseTest
+    {
+        [Test(Description = "SortableDataTables")]
+        public void SortableDataTablesTests()
+        {
+            driver.FindElement(By.LinkText("Sortable Data Tables")).Click();
+            var ElementOne = driver.FindElement(By.XPath("/html/body/div[2]/div/div/table[1]/tbody/tr[1]"));
+            string elementText = ElementOne.Text;
+            var ElementTwo = driver.FindElement(By.XPath("/html/body/div[2]/div/div/table[1]/tbody/tr[1]"));
+            string elementText2 = ElementTwo.Text;
+            var ElementThree = driver.FindElement(By.XPath("/html/body/div[2]/div/div/table[1]/tbody/tr[1]"));
+            string elementText3 = ElementThree.Text;
+            Console.WriteLine(elementText,elementText2,elementText3);
+
+
+        }
+    }
+}
