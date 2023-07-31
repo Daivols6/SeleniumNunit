1d3ea3b660ff0f23067c7b8bb207ea6e61e839f7
Daivols6 <daivols@internet.ru>
2023-07-31 19:42:32 +0300

    Add new project

---
+++ Diplom/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Diplom
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
