a2b6053420d7a7f1ea34b326e89604fb9cd4bf93
Daivols6 <daivols@internet.ru>
2023-08-17 21:46:43 +0300

    Add Test for Azure

---
+++ Diplom/Test/TestForAllure.cs
@@ -0,0 +1,19 @@
+﻿using Diplom.Diplom.PageObject;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIPLOM.Diplom.Test
+{
+    internal class TestForAllure
+    {
+        [Test]
+        public void AllureTestPassed()
+        {
+            Assert.Pass();
+        }
+    }
+}
